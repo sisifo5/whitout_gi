94f8811c8161807397e1291c1252494157c90dde
ArDroWeb <97034442+sisifo5@users.noreply.github.com>
2023-01-08 20:10:20 -0600

    Create Variabledos.java

---
+++ Variabledos.java
@@ -0,0 +1,49 @@
+package packone;
+
+/**
+ *
+ * @author paranoidandroid
+ */
+public class Variabledos extends Classp {
+
+    @Override
+    public void Letras() {
+        System.out.println("The open wounds I cut, now fester and succumb\n"
+                + "To insect infestation, bacterial infiltration\n"
+                + "On a microbiotic level, they grow and overtake\n"
+                + "A human piece of waste, an experiment of decay\n"
+                + "God of the fly, release your seed of filth\n"
+                + "To grow and conquer, on death, on myself\n"
+                + "\n"
+                + "Maggots, maggots!\n"
+                + "On my corpse feeding\n"
+                + "Seeping, pus does flow\n"
+                + "Rotten body\n"
+                + "\n"
+                + "The maggots feed and grow, the maggots feed and grow!\n"
+                + "\n"
+                + "Maggots, maggots!\n"
+                + "On my corpse feeding\n"
+                + "Seeping, pus does flow\n"
+                + "Rotten body\n"
+                + "\n"
+                + "The open wounds I cut, now fester and succumb\n"
+                + "To insect infestation, bacterial infiltration\n"
+                + "On a microbiotic level, they grow and overtake\n"
+                + "A human piece of waste, an experiment of decay\n"
+                + "God of the fly, release your seed of filth\n"
+                + "To grow and conquer, on death, on myself\n"
+                + "\n"
+                + "Maggots, maggots!\n"
+                + "On my corpse feeding\n"
+                + "Seeping, pus does flow\n"
+                + "Rotten body\n"
+                + "\n"
+                + "Seed of filth\n"
+                + "Seed of filth\n"
+                + "Seed of filth\n"
+                + "Seed of filth");
+
+    }
+
+}
