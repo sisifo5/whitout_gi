414aa28af444fecbdb2deef98c98f3f2a1ea4b77
ArDroWeb <97034442+sisifo5@users.noreply.github.com>
2023-01-08 20:09:08 -0600

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,58 @@
+package packone;
+
+public class Variable extends Classp {
+
+    @Override
+    public void Letras() {
+        System.out.println("I can't remember anything\n"
+                + "Can't tell if this is true or dream\n"
+                + "Deep down inside I feel the scream\n"
+                + "This terrible silence stops me\n"
+                + "\n"
+                + "Now that the war is through with me\n"
+                + "I'm waking up, I cannot see\n"
+                + "That there's not much left of me\n"
+                + "Nothing is real but pain now\n"
+                + "\n"
+                + "Hold my breath as I wish for death\n"
+                + "Oh please, God, wake me\n"
+                + "\n"
+                + "Back in the womb it's much too real\n"
+                + "In pumps life that I must feel\n"
+                + "But can't look forward to reveal\n"
+                + "Look to the time when I'll live\n"
+                + "\n"
+                + "Fed through the tube that sticks in me\n"
+                + "Just like a wartime novelty\n"
+                + "Tied to machines that make me be\n"
+                + "Cut this life off from me\n"
+                + "\n"
+                + "Hold my breath as I wish for death\n"
+                + "Oh please, God, wake me\n"
+                + "\n"
+                + "Now the world is gone, I'm just one\n"
+                + "Oh God, help me\n"
+                + "Hold my breath as I wish for death\n"
+                + "Oh please, God, help me\n"
+                + "\n"
+                + "Darkness\n"
+                + "Imprisoning me\n"
+                + "All that I see\n"
+                + "Absolute horror\n"
+                + "I cannot live\n"
+                + "I cannot die\n"
+                + "Trapped in myself\n"
+                + "Body my holding cell\n"
+                + "\n"
+                + "Landmine\n"
+                + "Has taken my sight\n"
+                + "Taken my speech\n"
+                + "Taken my hearing\n"
+                + "Taken my arms\n"
+                + "Taken my legs\n"
+                + "Taken my soul\n"
+                + "Left me with life in hell");
+
+    }
+
+}
