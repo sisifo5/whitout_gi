d03b3daa018b96b24a854ed3369fdbd4b8d75332
ArDroWeb <97034442+sisifo5@users.noreply.github.com>
2023-01-08 20:06:55 -0600

    Create Prin.java

---
+++ Prin.java
@@ -0,0 +1,14 @@
+package packone;
+
+/**
+ *
+ * @author paranoidandroid
+ */
+public class Prin {
+    public static void main(String [] args){
+        
+        Classp msj = new Variable();
+        msj.Lyrcs();
+    }
+    
+}
